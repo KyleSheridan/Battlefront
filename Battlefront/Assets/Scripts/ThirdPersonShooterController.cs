a931d44997ee1d44e28e4ccbf3dfd0faa7b4de21
Kyle Sheridan <55580454+KyleSheridan@users.noreply.github.com>
2022-11-23 17:45:45 +0000

    Player aim functionality

--- Battlefront/Assets/Scripts/ThirdPersonShooterController.cs
+++ Battlefront/Assets/Scripts/ThirdPersonShooterController.cs
@@ -9,6 +9,9 @@ public class ThirdPersonShooterController : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
     [SerializeField] private float normalSensitivity;
     [SerializeField] private float aimSensitivity;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    private Vector3 aimPoint;
+    private float aimRotationSpeed = 20f;
 
     private ThirdPersonController thirdPersonController;
     private StarterAssetsInputs starterAssetsInputs;
@@ -21,15 +24,37 @@ public class ThirdPersonShooterController : MonoBehaviour
 
     private void Update()
     {
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if(Physics.Raycast(ray, out RaycastHit hit, 999f, aimColliderLayerMask))
+        {
+            aimPoint = hit.point;
+        }
+
         if (starterAssetsInputs.aim)
         {
             aimVirtualCamera.gameObject.SetActive(true);
             thirdPersonController.SetSensitivity(aimSensitivity);
+            thirdPersonController.SetRotateOnMove(false);
+
+            Vector3 worldAimTarget = aimPoint;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, aimRotationSpeed * Time.deltaTime);
         }
         else
         {
             aimVirtualCamera.gameObject.SetActive(false);
             thirdPersonController.SetSensitivity(normalSensitivity);
+            thirdPersonController.SetRotateOnMove(true);
         }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(aimPoint, new Vector3(0.5f, 0.5f, 0.5f));
     }
 }
